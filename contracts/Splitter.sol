187760386df053d6d8ae889b65eb10fa9feedd18
gael blanchemain <gaelblanchemain@gaels-MacBook-Pro.local>
2017-09-04 17:47:10 -0400

    Added stuff

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -1,14 +1,30 @@
 pragma solidity ^0.4.4;
 
 contract Splitter {
-	address public bob;
-    address public carol;
+	address bob;
+    address carol;
+	uint share;
+// 	uint boBalance;
+	uint payment;
 
 	mapping (address => uint) balances;
 
-    function Splitter(address bobAddress, address carolAddress) {
-        bob = bobAddress;
-        carol = carolAddress;
+    function Splitter(address _bob, address _carol) {
+        bob = _bob;
+        carol = _carol;
+	}
+	
+    function Balance() returns(uint balance) {
+     return  bob.balance;
+	}
+
+	function Split(uint payment)
+	public
+	payable
+	returns (uint share)
+	 {
+		share = msg.value / 2;
+		return share;
 	}
 
 }
